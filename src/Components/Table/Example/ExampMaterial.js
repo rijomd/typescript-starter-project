b9efa5d73a4ab8451512914654e9c6e897aa8c32
rijomd70cr <rijo.md@codepointrs.com>
2023-03-17 17:39:25 +0530

    add changes in materia table

--- src/Components/Table/Example/ExampMaterial.js
+++ src/Components/Table/Example/ExampMaterial.js
@@ -1,4 +1,5 @@
-// import { Box } from "@mui/material";
+// import { Box, MenuItem } from "@mui/material";
+// import React, { useState } from "react";
 
 // import { MaterialReactTableComponent } from "../../../Components/Table";
 // import { dataArray } from "../../../Components/Table/data";
@@ -7,7 +8,30 @@
 // type Props = {};
 
 // const UserAdd = (props: Props) => {
-//   const action: any[] = [];
+//   let actions: Array<any> = [
+//     {
+//       label: "Refresh",
+//       icon: "",
+//       onClick: () => {
+//         console.log("hi");
+//       },
+//     },
+//     {
+//       label: "Add",
+//       icon: "",
+//       onClick: () => {
+//         console.log("hi");
+//       },
+//     },
+//     {
+//       label: "Delete",
+//       icon: "",
+//       onClick: () => {
+//         console.log("hi");
+//       },
+//     },
+//   ];
+//   // const [isLoading, setIsLoading] = useState(true);
 
 //   let columnData: any = [
 //     {
@@ -17,6 +41,17 @@
 //     {
 //       accessorKey: "year",
 //       header: "Year",
+//       renderColumnActionsMenuItems: ({ closeMenu, column, table }: any) => {
+//         return [
+//           <MenuItem
+//             onClick={() => {
+//               closeMenu();
+//             }}
+//           >
+//             Custom Menu Item 1
+//           </MenuItem>,
+//         ];
+//       }, //custom menu option
 //     },
 //     {
 //       accessorKey: "director",
@@ -25,10 +60,6 @@
 //     {
 //       accessorKey: "runtime",
 //       header: "Runtime",
-//       aggregationFn: "sum",
-//       AggregatedCell: ({ cell }: any) => (
-//         <div>Total Runtime: {cell.getValue()}</div>
-//       ),
 //     },
 //     {
 //       accessorKey: "actors",
@@ -43,22 +74,24 @@
 //   ];
 
 //   return (
-//     <PageLayout title="Add User" actions={action}>
+//     <PageLayout title="Add User" actions={[]}>
 //       <div>
 //         <MaterialReactTableComponent
 //           tableData={dataArray}
 //           columnData={columnData}
+//           tableActions={actions}
 //           getSubRows={(data: any) => data.general} //for getting subRows for expanding
 //           enableExpanding={false} //subRows for expanding conditions
 //           enableColumnResizing={true} //enable resize columns
 //           enableGrouping={false} // enabling grouping and arrows
-//           initialState={{
-//             density: "compact",
-//             expanded: false, //expand all groups by default
-//             grouping: ["country"], //an array of columns to group by by default (can be multiple)
-//             pagination: { pageIndex: 0, pageSize: 40 },
-//             sorting: [{ id: "state", desc: false }], //sort by state by default
-//           }}
+//           // initialState={{
+//           //   density: "compact",
+//           //   expanded: false, //expand all groups by default
+//           //   grouping: ["country"], //an array of columns to group by by default (can be multiple)
+//           //   pagination: { pageIndex: 0, pageSize: 40 },
+//           //   sorting: [{ id: "state", desc: false }], //sort by state by default
+//           // }}
+//           state={{ isLoading: false }}
 //         />
 //       </div>
 //     </PageLayout>
--- src/Components/Table/MaterialReactTable.tsx
+++ src/Components/Table/MaterialReactTable.tsx
@@ -1,15 +1,18 @@
 import { useMemo, useState, useEffect } from "react";
 import MaterialReactTable, { MRT_ColumnDef } from "material-react-table";
+import { FormButton } from "../FormElements/FormButton";
 
 type Props = {
   tableData: any[];
   columnData: any[];
+  tableActions: any[];
   [detailedProps: string]: any;
 };
 
 export const MaterialReactTableComponent = ({
   tableData,
   columnData,
+  tableActions,
   ...detailedProps
 }: Props) => {
   const [data, setData] = useState<any>([]);
@@ -20,12 +23,33 @@ export const MaterialReactTableComponent = ({
   }, [tableData]);
   const columns = useMemo<MRT_ColumnDef<any>[]>(() => columnData, [columnData]);
 
+  const renderActions = () => {
+    let myArray = [];
+    for (let item of tableActions) {
+      myArray.push(
+        <FormButton
+          style={{ mr: 2, fontSize: 13 }}
+          fullWidth={false}
+          loading={false}
+          onClick={() => item.onClick({})}
+          color={item.color}
+          variant="contained"
+        >
+          {item.label + " "} {item.icon}
+        </FormButton>
+      );
+    }
+    return <div style={{ display: "flex" }}>{myArray}</div>;
+  };
+
   return (
-    <MaterialReactTable
-      columns={columns}
-      data={data}
-      {...detailedProps}
-    />
+    <div>
+      <MaterialReactTable
+        columns={columns}
+        data={data}
+        renderTopToolbarCustomActions={() => renderActions()}
+        {...detailedProps}
+      />
+    </div>
   );
 };
-
